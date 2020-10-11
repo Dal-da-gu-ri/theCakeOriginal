2e246c942726b359851e77c437f2c51bc63203bc
Sohyun Park <carriesohyun@gmail.com>
2020-10-12 02:42:13 +0900

    editing baker view

--- baker/views.py
+++ baker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from thecakeapp.models import Orderer, Order, Store, Baker, Review, Option, DetailedOption, Cake
+from django.contrib.auth.models import User
+from django.contrib import auth
 # Create your views here.
 
 from django.http import HttpResponse
@@ -7,7 +9,16 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Index page!")
 
+def signUp(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+
+
 def login(request):
+    person = Baker
+    person.email = request.GET('email')
+    person.password = request.GET('password')
+
     return render(request, 'baker/login_baker.html')
 
 def valid(request): #사업자번호확인 -> join
