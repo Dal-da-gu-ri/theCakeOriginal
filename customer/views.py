4af3136a26216d66b243cc6dddd3fb24df240faf
Sohyun Park <carriesohyun@gmail.com>
2020-10-17 18:08:10 +0900

    orderer views 수정

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from thecakeapp.models import Orderer, Order, Store, Baker, Review, Option, DetailedOption, Cake
-
+from django.contrib.auth.hashers import make_password,check_password
 # Create your views here.
 def main(request):
     return render(request, 'customer/main_customer.html')
@@ -18,6 +18,7 @@ def emailValid(request):
             # return 다른 이메일을 입력해주세요
             return render(request, 'customer/login_customer.html')
 
+"""
 def passwordCheck(request):
     #비밀번호 입력 확인
     if request.method == 'POST':
@@ -30,7 +31,9 @@ def passwordCheck(request):
         else:
             # 입력한 비밀번호를 다시 확인해주세요
             return render(request, 'customer/login_customer.html')
+"""
 
+"""
 def signup(request):
     if request.method == 'POST':
         if Orderer.objects.filter(email__exact=request.POST["email"]) == False:
@@ -43,11 +46,57 @@ def signup(request):
                 customerUser.save()
 
                 return render(request, 'customer/test.html')
-
+"""
 
 
 def signUp(request):
-    return render(request, 'customer/signUp_customer.html')
+    #validemail = None
+    res_data = {}
+    if request.method == "GET":
+        return render(request,'customer/signUp_customer.html')
+    elif request.method == "POST" and 'btn-email' in request.POST:
+        email_customer = request.POST.get('email_customer', None)
+        if Orderer.objects.filter(email = email_customer).exists():
+            #email_fail
+            #validemail = False
+            res_data['check_email']="이메일 중복"
+            #return render(request,'customer/signUp_customer.html', {'check_email':validemail})
+        else:
+            #email_success
+            #validemail = True
+            res_data['check_email'] = "이메일 유효"
+            #render(request,'customer/signUp_customer.html',  {'check_email':validemail})
+    elif request.method == "POST" and 'submit' in request.POST:
+        name_customer = request.POST.get('name_customer',None)
+        email_customer = request.POST.get('email_customer',None)
+        password_customer = request.POST.get('password_customer',None)
+        re_password_customer = request.POST.get('re-password_customer',None)
+        phoneNum_customer = request.POST.get('phoneNum_customer',None)
+
+        """
+        if not name_customer and email_customer and password_customer and re_password_customer and phoneNum_customer:
+            # 모든 값을 입력해야한다는 경고문
+        else:
+            customer = Orderer(
+                email = email_customer,
+                name = name_customer,
+                phoneNum = phoneNum_customer,
+                password = password_customer
+            )
+            
+            customer.save()
+        
+        """
+        customer = Orderer(
+            email=email_customer,
+            name=name_customer,
+            phoneNum=phoneNum_customer,
+            password=password_customer
+        )
+
+        customer.save()
+
+        return render(request, 'customer/signUp_customer.html')
 
 def login(request):
     return render(request, 'customer/login_customer.html')
